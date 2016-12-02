6ece58dcc63666b0a78676df5c00277d4db3ffa6
Quinn Zepeda <developerquinnz@gmail.com>
2016-12-02 09:55:25 -0800

    TakeAllthefiles

---
+++ MovingOn/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+
+        DirectoryInfo Source = new DirectoryInfo(@"c:\users\student\documents\visual studio 2015\Projects\MovingOn\MovingOn\A\");
+        DirectoryInfo Destination = new DirectoryInfo(@"c:\users\student\documents\visual studio 2015\Projects\MovingOn\MovingOn\B\");
+        foreach (var File in Source.EnumerateFiles())
+        {
+            if (isLessThanDayOld(File.LastWriteTime))
+            {
+                File.CopyTo((Destination + File.Name));
+            }
+        }
+
+    }
+    public static bool isLessThanDayOld(DateTime LastMod)
+    {
+
+        var IGetMoved = DateTime.Now.AddDays(1);
+
+        
+        DateTime TooOld = DateTime.Now.AddDays(-1);
+        
+        
+
+
+
+        return LastMod > TooOld;
+    }
+
+}
